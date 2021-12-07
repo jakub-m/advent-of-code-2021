ca24141df6d7a78530d40e09a1c2eeca78f815e9
Jakub Mikians <jakub.mikians@gmail.com>
2021-12-07 21:22:54 +0100

    ho ho ho

---
+++ 07/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "advent"
+
+func Calc(input string) (int, error) {
+	initialPositions, err := advent.ReadIntsFromStringSep(input, ",")
+	if err != nil {
+		return 0, err
+	}
+
+	min, max := advent.MinInt(initialPositions), advent.MaxInt(initialPositions)
+
+	minPositionCost := -1
+
+	for tentativePos := min; tentativePos <= max; tentativePos++ {
+		cost := calcMoveCost(initialPositions, tentativePos)
+		if tentativePos == min || cost < minPositionCost {
+			minPositionCost = cost
+		}
+	}
+	return minPositionCost, nil
+}
+
+func calcMoveCost(positions []int, targetPos int) int {
+	cost := 0
+	for _, pos := range positions {
+		cost += advent.AbsInt(pos - targetPos)
+	}
+	return cost
+}
