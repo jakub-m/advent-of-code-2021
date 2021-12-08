67ac491fb71315283897d07678d0b724f011048f
Jakub Mikians <jakub.mikians@gmail.com>
2021-12-08 22:03:30 +0100

    ho ho ho

--- 08/main.go
+++ 08/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"advent"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -20,34 +21,15 @@ func Calc(r io.Reader) (int, error) {
 }
 
 func calculateScoreFromString(line string) int {
-	score := 0
 	input := parseLine(line)
-	in := input.checkSegments
-	// digit 1
-	if selected := selectSegments(in, func(s segment) bool {
-		return len(s) == 2
-	}); len(selected) > 0 {
-		score += len(selected)
-	}
-	// digit 4
-	if selected := selectSegments(in, func(s segment) bool {
-		return len(s) == 4
-	}); len(selected) > 0 {
-		score += len(selected)
-	}
-	// digit 7
-	if selected := selectSegments(in, func(s segment) bool {
-		return len(s) == 3
-	}); len(selected) > 0 {
-		score += len(selected)
-	}
-	// digit 8
-	if selected := selectSegments(in, func(s segment) bool {
-		return len(s) == 7
-	}); len(selected) > 0 {
-		score += len(selected)
+	digitMap := decodeDigitsFromSegments(input.allSegments)
+	score := 0
+	for _, s := range input.checkSegments {
+		score = score * 10
+		score += digitMap.decode(s)
 	}
 	return score
+
 }
 
 type parsedInput struct {
@@ -56,6 +38,21 @@ type parsedInput struct {
 }
 
 type segment string
+
+func (s segment) subCount(other segment) int {
+	count := 0
+	for _, cs := range s {
+		count++
+		for _, co := range other {
+			if cs == co {
+				count--
+				break
+			}
+
+		}
+	}
+	return count
+}
 
 func parseLine(line string) parsedInput {
 	// todo sort strings
@@ -67,6 +64,91 @@ func parseLine(line string) parsedInput {
 	}
 }
 
+func parseSegments(input string) []segment {
+	segments := []segment{}
+	for _, s := range strings.Split(input, " ") {
+		segments = append(segments, segment(s))
+	}
+	return segments
+}
+
+type digitEncoding map[segment]int
+
+func (d digitEncoding) decode(segment segment) int {
+
+	for k, v := range d {
+		if k.subCount(segment) == 0 && segment.subCount(k) == 0 {
+			return v
+		}
+	}
+	panic(fmt.Sprintf("cannot decode: %s with %+v", segment, d))
+}
+
+func decodeDigitsFromSegments(allDigits []segment) digitEncoding {
+	digitEncoding := make(map[segment]int)
+
+	d1 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 2
+	})
+
+	d4 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 4
+	})
+
+	d7 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 3
+	})
+
+	d8 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 7
+	})
+
+	d3 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 5 && s.subCount(d7) == 2
+	})
+
+	d6 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 6 && s.subCount(d3) == 1
+	})
+
+	d5 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 5 && d6.subCount(s) == 1 && d1.subCount(s) == 1
+	})
+
+	d9 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 6 && s.subCount(d3) == 1 && d3.subCount(s) == 0
+	})
+
+	d2 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 5 && d4.subCount(s) == 2 && s.subCount(d4) == 3
+	})
+
+	d0 := selectOnly(allDigits, func(s segment) bool {
+		return len(s) == 6 && s.subCount(d7) == 3 && d7.subCount(s) == 0 && d4.subCount(s) == 1
+	})
+
+	digitEncoding[d0] = 0
+	digitEncoding[d1] = 1
+	digitEncoding[d2] = 2
+	digitEncoding[d3] = 3
+	digitEncoding[d4] = 4
+	digitEncoding[d5] = 5
+	digitEncoding[d6] = 6
+	digitEncoding[d7] = 7
+	digitEncoding[d8] = 8
+	digitEncoding[d9] = 9
+
+	return digitEncoding
+}
+
+func selectOnly(segments []segment, cond func(segment) bool) segment {
+	if selected := selectSegments(segments, cond); len(selected) == 1 {
+		return selected[0]
+	} else {
+		panic(fmt.Sprintf("selected: %v", selected))
+	}
+}
+
 func selectSegments(segments []segment, cond func(segment) bool) []segment {
 	selected := []segment{}
 	for _, s := range segments {
@@ -75,12 +157,4 @@ func selectSegments(segments []segment, cond func(segment) bool) []segment {
 		}
 	}
 	return selected
-}
-
-func parseSegments(input string) []segment {
-	segments := []segment{}
-	for _, s := range strings.Split(input, " ") {
-		segments = append(segments, segment(s))
-	}
-	return segments
 }
--- 08/main_test.go
+++ 08/main_test.go
@@ -13,16 +13,16 @@ func TestCalc1(t *testing.T) {
 		input string
 		score int
 	}{
-		// {"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", 2},
-		// {"edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", 3},
-		{"fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", 3},
-		// {"fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", 1},
-		// {"aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", 3},
-		// {"fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", 4},
-		// {"dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", 3},
-		// {"bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", 1},
-		// {"egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", 4},
-		// {"gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", 2},
+		{"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", 8394},
+		{"edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", 9781},
+		{"fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", 1197},
+		// {"fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", 9361},
+		// {"aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", 4873},
+		{"fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", 8418},
+		{"dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", 4548},
+		// {"bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", 1625},
+		{"egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", 8717},
+		{"gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", 4315},
 	}
 
 	for _, tc := range tcs {
@@ -38,7 +38,7 @@ func TestCalcInput1(t *testing.T) {
 	assert.NoError(t, err)
 	v, err := Calc(f)
 	assert.NoError(t, err)
-	assert.Equal(t, 26, v)
+	assert.Equal(t, 61229, v)
 }
 
 func TestCalcInput2(t *testing.T) {
@@ -47,4 +47,11 @@ func TestCalcInput2(t *testing.T) {
 	v, err := Calc(f)
 	assert.NoError(t, err)
 	assert.Equal(t, 26, v)
+}
+
+func TestCalc3(t *testing.T) {
+	input := "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+	v, err := Calc(strings.NewReader(input))
+	assert.NoError(t, err)
+	assert.Equal(t, 5353, v)
 }
