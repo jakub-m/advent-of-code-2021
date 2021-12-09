976cd4a28937a3f454ec723f11838d3ab8bae03f
Jakub Mikians <jakub.mikians@gmail.com>
2021-12-09 08:35:16 +0100

    ho ho ho

--- 09/main.go
+++ 09/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "io"
+import (
+	"advent"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+type coord struct {
+	x, y int
+}
+
+type landscape map[coord]int
 
 func Calc(r io.Reader) (int, error) {
-	return 0, nil
+	lines, err := advent.ReadLinesTrim(r)
+	if err != nil {
+		return 0, err
+	}
+
+	landscape := make(landscape)
+	for iRow, row := range lines {
+		for iCol, char := range row {
+			height, err := strconv.Atoi(fmt.Sprintf("%c", char))
+			if err != nil {
+				return 0, err
+			}
+			c := coord{iCol, iRow}
+			fmt.Printf("%v=%d\n", c, height)
+			landscape[c] = height
+		}
+	}
+
+	score := 0
+	for coord, height := range landscape {
+		heights := landscape.getMany(landscape.getNeighbors(coord))
+		if height == advent.MinInt(heights) {
+			score += height + 1
+		}
+	}
+
+	return score, nil
+}
+
+func (d landscape) getMany(coords []coord) []int {
+	values := []int{}
+	for _, c := range coords {
+		if val, ok := d[c]; ok {
+			values = append(values, val)
+		} else {
+			panic(fmt.Sprintf("No such coord: %+v", c))
+		}
+	}
+	return values
+}
+
+func (d landscape) getNeighbors(c coord) []coord {
+	neighbors := []coord{}
+
+	for _, c := range []coord{
+		{x: c.x + 1, y: c.y + 1},
+		{x: c.x - 1, y: c.y + 1},
+		{x: c.x + 1, y: c.y - 1},
+		{x: c.x - 1, y: c.y - 1},
+	} {
+		if _, ok := d[c]; ok {
+			neighbors = append(neighbors, c)
+		}
+	}
+	return neighbors
 }
--- 09/main_test.go
+++ 09/main_test.go
@@ -7,8 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var input1 = `2199943210
+3987894921
+9856789892
+8767896789
+9899965678`
+
 func TestCalc1(t *testing.T) {
-	v, err := Calc(strings.NewReader(""))
+	v, err := Calc(strings.NewReader(input1))
 	assert.NoError(t, err)
-	assert.Equal(t, 0, v)
+	assert.Equal(t, 15, v)
 }
