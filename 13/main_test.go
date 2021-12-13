0e7bf09a7a18772168402c3ce20de84fa6b55a0c
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2021-12-13 22:25:03 +0100

    ho ho ho

--- 13/main_test.go
+++ 13/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -12,5 +13,15 @@ func TestCalc1(t *testing.T) {
 	assert.NoError(t, err)
 	v, err := Calc(f)
 	assert.NoError(t, err)
+	fmt.Print(v)
+	assert.Equal(t, "something", v)
+}
+
+func TestCalc2(t *testing.T) {
+	f, err := os.Open("input2")
+	assert.NoError(t, err)
+	v, err := Calc(f)
+	assert.NoError(t, err)
+	fmt.Print(v)
 	assert.Equal(t, "something", v)
 }
