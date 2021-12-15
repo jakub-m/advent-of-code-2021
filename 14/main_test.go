6c339a279118fc30126f6a9ea223891550f6a7af
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2021-12-15 09:00:32 +0100

    ho ho ho

--- 14/main_test.go
+++ 14/main_test.go
@@ -14,3 +14,11 @@ func TestCalc1(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 1588, v)
 }
+
+func TestCalc2(t *testing.T) {
+	f, err := os.Open("input2")
+	assert.NoError(t, err)
+	v, err := Calc(f, 10)
+	assert.NoError(t, err)
+	assert.Equal(t, 1588, v)
+}
