c7e81354e883459e4ac27565f0982040a457fcac
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2021-12-17 00:18:52 +0100

    ho ho ho

--- 16/main.go
+++ 16/main.go
@@ -4,22 +4,46 @@ import (
 	"advent"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-func Calc(raw string) (int, error) {
-
-	p := decodePacketRawBin(raw)
+func Calc(rawHex string) (int, error) {
+	bin, err := hexToBin(rawHex)
+	advent.Printf("hex %s\n", rawHex)
+	advent.Printf("binary %s\n", bin)
+	if err != nil {
+		return 0, err
+	}
+	p, _ := decodePacketRawBin(bin)
 	advent.Printf("%v", p)
 
-	return 0, nil
+	var sumVerRec func(p packet) int
+
+	sumVerRec = func(p packet) int {
+		switch p := p.(type) {
+		case literalPacket:
+			return p.getVersion()
+		case operatorPacket:
+			v := p.version
+			for _, ch := range p.packets {
+				v += sumVerRec(ch)
+			}
+			return v
+		default:
+			panic(p)
+		}
+	}
+
+	return sumVerRec(p), nil
 }
 
-type packet struct {
-	version int
-	value   int
+type packet interface {
+	getVersion() int
 }
 
-const Literal = 4
+const (
+	Literal = 4
+)
 
 func getVersionAndType(rawBin string) (int, int, string) {
 	version := binToInt(rawBin[0:3])
@@ -30,21 +54,35 @@ func getVersionAndType(rawBin string) (int, int, string) {
 	case Literal:
 		return version, Literal, rawBin
 	default:
-		panic(fmt.Sprintf("unexpected type for: %s", rawBin))
+		// operator
+		return version, typeId, rawBin
 	}
 }
 
-func decodePacketRawBin(rawBin string) packet {
+func decodePacketRawBin(rawBin string) (packet, string) {
 	_, typeId, _ := getVersionAndType(rawBin)
 	if typeId == Literal {
-		p, _ := decodeLiteral(rawBin)
-		return p
+		return decodeLiteral(rawBin)
 	} else {
-		panic(fmt.Sprintf("expected type id 4: %s", rawBin))
+		return decodeOperator(rawBin)
 	}
 }
 
-func decodeLiteral(rawBin string) (packet, string) {
+type literalPacket struct {
+	version int
+	value   int
+}
+
+func (p literalPacket) getVersion() int {
+	return p.version
+}
+
+func (p literalPacket) String() string {
+	return fmt.Sprintf("lit:%d,%d", p.version, p.value)
+}
+
+func decodeLiteral(rawBin string) (literalPacket, string) {
+	advent.Printf("decodeLiteral %s\n", rawBin)
 	version, typeId, rawBin := getVersionAndType(rawBin)
 	advent.Assertf(typeId == Literal, "unexpected type: %d, %s", typeId, rawBin)
 	subBin := ""
@@ -57,9 +95,70 @@ func decodeLiteral(rawBin string) (packet, string) {
 			break
 		}
 	}
-	val := binToInt(subBin)
-	p := packet{version: version, value: val}
+	//val := binToInt(subBin)
+	val := -1
+	p := literalPacket{version: version, value: val}
 	return p, rawBin
+}
+
+type operatorPacket struct {
+	version int
+	packets []packet
+}
+
+func (p operatorPacket) getVersion() int {
+	return p.version
+}
+
+func (p operatorPacket) String() string {
+	packets := []string{}
+	for _, p := range p.packets {
+		packets = append(packets, fmt.Sprint(p))
+	}
+	return fmt.Sprintf("op:%d,[%s]", p.version, strings.Join(packets, ", "))
+}
+
+func decodeOperator(rawBin string) (operatorPacket, string) {
+	advent.Printf("decodeOperator %s\n", rawBin)
+	version, _, rawBin := getVersionAndType(rawBin)
+	lengthTypeId, rawBin := rawBin[0], rawBin[1:]
+
+	var finalRawBin string
+	packets := []packet{}
+	if lengthTypeId == '0' {
+		// next 15 are tot length of other subpackets
+		s, rawBin := rawBin[0:15], rawBin[15:]
+		nbitsOfNextPackets := binToInt(s)
+		advent.Printf("nbitsOfNextPackets %s %d\n", s, nbitsOfNextPackets)
+		breakAtRawbinLen := len(rawBin) - nbitsOfNextPackets
+		advent.Assertf(breakAtRawbinLen > 0, "bullshit expected length: %d", breakAtRawbinLen)
+		for {
+			p, rawBin2 := decodePacketRawBin(rawBin)
+			rawBin = rawBin2
+			packets = append(packets, p)
+			if len(rawBin) == breakAtRawbinLen {
+				finalRawBin = rawBin
+				break
+			}
+			advent.Assertf(len(rawBin) >= breakAtRawbinLen, "Expected to finish at len %d but is already %d", len(rawBin), breakAtRawbinLen)
+		}
+	} else {
+		// 11 bits are num of subpackets
+		s, rawBin := rawBin[0:11], rawBin[11:]
+		numOfNextPackets := binToInt(s)
+		advent.Printf("numOfNextPackets %d\n", numOfNextPackets)
+		for {
+			p, rawBin2 := decodePacketRawBin(rawBin)
+			rawBin = rawBin2
+			packets = append(packets, p)
+			if len(packets) == numOfNextPackets {
+				finalRawBin = rawBin
+				break
+			}
+		}
+	}
+
+	return operatorPacket{version: version, packets: packets}, finalRawBin
 }
 
 func binToInt(bin string) int {
@@ -68,4 +167,16 @@ func binToInt(bin string) int {
 		panic(err)
 	}
 	return int(i)
+}
+
+func hexToBin(hex string) (string, error) {
+	b := ""
+	for _, h := range hex {
+		d, err := strconv.ParseInt(fmt.Sprintf("%c", h), 16, 32)
+		if err != nil {
+			return "", err
+		}
+		b += fmt.Sprintf("%04s", strconv.FormatInt(d, 2))
+	}
+	return b, nil
 }
